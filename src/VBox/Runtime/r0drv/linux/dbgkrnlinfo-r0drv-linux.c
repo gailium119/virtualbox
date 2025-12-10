55e0f52217892c1b6f9009d5e553a6480ed6e765
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-12-10 00:06:30 +0000

    IPRT/r0drv/linux: Stub the debug code if not supported to avoid linking errors with RTR0DbgKrnlInfoGetSymbol.
    svn:sync-xref-src-repo-rev: r171748

--- src/VBox/Runtime/r0drv/linux/dbgkrnlinfo-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/dbgkrnlinfo-r0drv-linux.c
@@ -1,4 +1,4 @@
-/* $Id: dbgkrnlinfo-r0drv-linux.c 112075 2025-12-09 23:55:12Z knut.osmundsen@oracle.com $ */
+/* $Id: dbgkrnlinfo-r0drv-linux.c 112076 2025-12-10 00:06:30Z knut.osmundsen@oracle.com $ */
 /** @file
  * IPRT - Kernel Debug Information, R0 Driver, Linux.
  */
@@ -315,5 +315,36 @@ RTR0DECL(int) RTR0DbgKrnlInfoQuerySymbol(RTDBGKRNLINFO hKrnlInfo, const char *ps
     return VERR_SYMBOL_NOT_FOUND;
 }
 
-#endif /* RTLNX_VER_MIN(6,7,0) || defined(IN_RING3) */
+#else  /* !RTLNX_VER_MIN(6,7,0) && !defined(IN_RING3) */
+
+/*
+ * Stubs to prevent linking issues with RTR0DbgKrnlInfoGetSymbol and such.
+ */
+
+RTR0DECL(int) RTR0DbgKrnlInfoOpen(PRTDBGKRNLINFO phKrnlInfo, uint32_t fFlags)
+{
+    RT_NOREF(phKrnlInfo, fFlags);
+    return VERR_NOT_IMPLEMENTED;
+}
+
+RTR0DECL(uint32_t) RTR0DbgKrnlInfoRetain(RTDBGKRNLINFO hKrnlInfo)
+{
+    RT_NOREF(hKrnlInfo);
+    return UINT32_MAX;
+}
+
+RTR0DECL(uint32_t) RTR0DbgKrnlInfoRelease(RTDBGKRNLINFO hKrnlInfo)
+{
+    RT_NOREF(hKrnlInfo);
+    return UINT32_MAX;
+}
+
+RTR0DECL(int) RTR0DbgKrnlInfoQuerySymbol(RTDBGKRNLINFO hKrnlInfo, const char *pszModule,
+                                         const char *pszSymbol, void **ppvSymbol)
+{
+    RT_NOREF(hKrnlInfo, pszModule, pszSymbol, ppvSymbol);
+    return VERR_NOT_IMPLEMENTED;
+}
+
+#endif /* !RTLNX_VER_MIN(6,7,0) && !defined(IN_RING3) */
 
