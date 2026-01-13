b3a2682b2e86cd3d6c6eb9a7311ae74e9a02a68b
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-01-13 15:40:56 +0000

    Additions: Linux: vboxvideo: Fixes for RHEL 10.1 kernel, bugref:10950, github:gh-455.
    svn:sync-xref-src-repo-rev: r172226

--- src/VBox/Additions/linux/drm/vbox_main.c
+++ src/VBox/Additions/linux/drm/vbox_main.c
@@ -1,4 +1,4 @@
-/* $Id: vbox_main.c 112507 2026-01-13 15:06:08Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_main.c 112520 2026-01-13 15:40:56Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -180,7 +180,7 @@ int vbox_framebuffer_init(struct drm_device *dev,
 {
 	int ret;
 
-#if RTLNX_VER_MIN(6,17,0) || RTLNX_RHEL_RANGE(9,8, 9,99)
+#if RTLNX_VER_MIN(6,17,0) || RTLNX_RHEL_RANGE(9,8, 9,99) || RTLNX_RHEL_RANGE(10,1, 10,99)
 	const struct drm_format_info *format = drm_get_format_info(dev, mode_cmd->pixel_format, 0);
 	drm_helper_mode_fill_fb_struct(dev, &vbox_fb->base, format, mode_cmd);
 #elif RTLNX_VER_MIN(4,11,0) || RTLNX_RHEL_MAJ_PREREQ(7,5)
@@ -201,7 +201,7 @@ int vbox_framebuffer_init(struct drm_device *dev,
 static struct drm_framebuffer *vbox_user_framebuffer_create(
 		struct drm_device *dev,
 		struct drm_file *filp,
-#if RTLNX_VER_MIN(6,17,0) || RTLNX_RHEL_RANGE(9,8, 9,99)
+#if RTLNX_VER_MIN(6,17,0) || RTLNX_RHEL_RANGE(9,8, 9,99) || RTLNX_RHEL_RANGE(10,1, 10,99)
 		const struct drm_format_info *info,
 #endif
 #if RTLNX_VER_MIN(4,5,0) || RTLNX_RHEL_MAJ_PREREQ(7,3)
